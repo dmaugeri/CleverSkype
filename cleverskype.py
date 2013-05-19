a3e8b632ccb34368eea64c9b3f95af847cd6fdee
Daniel Maugeri <dcc.maugeri@gmail.com>
2013-05-19 14:51:58 -0400

    added print statements for debugging purposes

--- cleverskype.py
+++ cleverskype.py
@@ -15,15 +15,19 @@ class CleverSkype:
         #keeps track of whether the first message has been sent or not
         self.messageIndex = 0;
         self.running = False
+        pprint("Creating skype...")
         self.skype = Skype4Py.Skype()
         self.skype.Attach()
+        pprint("Skype attached.")
         
     def start(self):
+        pprint("Starting Cleverbot to receive messages")
         self.running = True
         self.skype.OnMessageStatus = self.handleMessages
     
     #stops responding to messages
     def stop(self):
+        pprint("Stopping Cleverbot from receiving messages")
         self.messageIndex = 0
         self.running = False
         self.skype.OnMessageStatus = self.doNothing     
@@ -44,13 +48,18 @@ class CleverSkype:
         if status == "READ":
             return
         
+        
         #if it is the first message being sent...
         if self.messageIndex == 0:
+            pprint("First Message Received..")
             msg.Chat.SendMessage('%s isn\'t here right now, but feel free to talk to me, Cleverbot, in the mean time' 
                                  % (self.skype.CurrentUser.FullName))
+            pprint("First Message Sent")
         else:
+            pprint("Asking Cleverbot...")
             response = self.cb.Ask(msg.Body) 
             msg.Chat.SendMessage(response)
+            pprint("Cleverbot's response sent.")
         
         self.messageIndex = self.messageIndex + 1
         
@@ -68,12 +77,11 @@ def main():
             if cleverSkype.running:
                 pprint("stopping...")
                 cleverSkype.stop()
-            time.sleep(1)
         else:
             if not cleverSkype.running:
                 pprint("starting...")
                 cleverSkype.start()
-            time.sleep(1)  
+        time.sleep(1)  
     
 if __name__ == '__main__':
     main()
